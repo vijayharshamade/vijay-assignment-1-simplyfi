a90e20aec534dabdac1151b62f34dafabb190b51
Vijay Harsha Made <87754774+vijayharshamade@users.noreply.github.com>
2023-01-06 11:47:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      {/* <p>Website Logo</p> */}
+      <img
+        src="https://res.cloudinary.com/vijayharshamade/image/upload/v1672985088/MicrosoftTeams-image_meh2vs.jpg"
+        className="website-logo"
+        alt="website logo"
+      />
+    </>
+  );
+}
